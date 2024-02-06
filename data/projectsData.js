35d76e8be694017f0ab48a2b3b8f1a4cdc3afa3d
Hamza Haroon <95119705+thegr1ffyn@users.noreply.github.com>
2024-02-06 22:08:30 +0500

    Add files via upload

---
+++ data/projectsData.js
@@ -0,0 +1,10 @@
+const projectsData = [
+  {
+    title: 'Project',
+    description: `Currently in Development`,
+    imgSrc: '/static/images/cat.jpg',
+    href: 'https://github.com/thegr1ffyn',
+  }
+]
+
+export default projectsData
