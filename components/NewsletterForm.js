57c7192a6df6c7a15f28452ba984797a6d194459
Hamza Haroon <95119705+thegr1ffyn@users.noreply.github.com>
2024-02-06 22:55:00 +0500

    Add files via upload

---
+++ components/NewsletterForm.js
@@ -0,0 +1,52 @@
+import { useRef, useState } from 'react'
+
+import siteMetadata from '@/data/siteMetadata'
+
+const NewsletterForm = ({ title = 'Subscribe to the newsletter' }) => {
+  const inputEl = useRef(null)
+  const [error, setError] = useState(false)
+  const [message, setMessage] = useState('')
+  const [subscribed, setSubscribed] = useState(false)
+
+  const subscribe = async (e) => {
+    e.preventDefault()
+
+    const res = await fetch(`/api/${siteMetadata.newsletter.provider}`, {
+      body: JSON.stringify({
+        email: inputEl.current.value,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      method: 'POST',
+    })
+
+    const { error } = await res.json()
+    if (error) {
+      setError(true)
+      setMessage('Your e-mail address is invalid or you are already subscribed!')
+      return
+    }
+
+    inputEl.current.value = ''
+    setError(false)
+    setSubscribed(true)
+    setMessage('Successfully! 🎉 You are now subscribed.')
+  }
+
+  return (
+    <div>
+      <p></p>
+    </div>
+  )
+}
+
+export default NewsletterForm
+
+export const BlogNewsletterForm = ({ title }) => (
+  <div className="flex items-center justify-center">
+    <div className="bg-gray-100 p-6 dark:bg-gray-800 sm:px-14 sm:py-8">
+      {/* <NewsletterForm title={title} /> */}
+    </div>
+  </div>
+)
